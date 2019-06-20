6166282732cae002fa2ce1015d4d2f5bb2c0c0bf
Fabian van der Hoeven <fabian.vanderhoeven@vermont24-7.com>
2019-06-20 10:51:24 +0200

    Added bootstraps for puppetmaster and postgresql_master

--- bootstrap_postgresql_master.sh
+++ bootstrap_postgresql_master.sh
@@ -15,10 +15,29 @@ systemctl restart rsyslog
 yum -y install http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 #Install puppet
 yum -y install puppet-agent-1.10.12
+source /etc/profile.d/puppet-agent.sh
+
+sudo bash -c 'cat << EOF > /etc/puppetlabs/puppet/puppet.conf
+[main]
+vardir = /var/lib/puppet
+logdir = /var/log/puppet
+rundir = /var/run/puppet
+ssldir = $vardir/ssl
+server = puppetmaster.mdt-cmc.local
+
+[agent]
+report          = true
+ignoreschedules = true
+daemon          = false
+environment     = production
+EOF'
 
 #Firewall prereq
 yum -y remove firewalld
 yum -y install iptables-services
 
 #Apply puppet manifest
-/opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests/site.pp
+#local manifest
+#/opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests/site.pp
+#manifest on puppetmaster
+puppet agent -t
